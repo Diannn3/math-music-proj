deff6d53c9d53df89ffe7c2a2ece42959d6721c9
Aedrian Ponce <aedrianponce1203@gmail.com>
2026-09-18 12:21:42 +0800

    feat(perf): export visual quality policy

--- src/visual/index.ts
+++ src/visual/index.ts
@@ -4,3 +4,4 @@ export { default as OrbitHistory } from './OrbitHistory';
 export { default as CobwebPlot } from './CobwebPlot';
 export { default as LyapunovStrip } from './LyapunovStrip';
 export * from './mathLensData';
+export * from './quality';
