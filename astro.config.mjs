42cdefd53ec0366009487226777e90210d87f6f0
Aedrian Ponce <aedrianponce1203@gmail.com>
2026-09-18 15:56:34 +0800

    release(web): define canonical public site origin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: process.env.PUBLIC_SITE_URL ?? 'https://bifurcate-math-music.vercel.app',
   integrations: [react()],
   vite: {
     build: {
