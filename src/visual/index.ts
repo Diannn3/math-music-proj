a2b2b750a7c7235eb786061d50321bac21160e9b
Aedrian Ponce <aedrianponce1203@gmail.com>
2026-09-18 12:15:57 +0800

    feat(math-lens): export mathematical visual modules

--- src/visual/index.ts
+++ src/visual/index.ts
@@ -1,3 +1,6 @@
 export * from './coordinates';
 export { default as BifurcationField } from './BifurcationField';
 export { default as OrbitHistory } from './OrbitHistory';
+export { default as CobwebPlot } from './CobwebPlot';
+export { default as LyapunovStrip } from './LyapunovStrip';
+export * from './mathLensData';
